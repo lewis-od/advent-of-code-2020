65aa504db89e5a570572573ae9766e0932ef9a44
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2020-12-11 20:57:10 +0000

    Finish day 7 part 1

--- day07/main.go
+++ day07/main.go
@@ -25,35 +25,59 @@ type BagGraph struct {
 	adjacencyMatrix [][]int
 }
 
-func (g *BagGraph) addEdge(from, to string, weight int) {
+func (g *BagGraph) AddEdge(from, to string, weight int) {
 	fromId := g.bagIds[from]
 	toId := g.bagIds[to]
 	g.adjacencyMatrix[toId][fromId] = weight
 }
 
-func newGraph(bags []string) BagGraph {
-	numNodes := len(bags)
-	adjacencyMatrix := make([][]int, numNodes)
-	for i := range adjacencyMatrix {
-		adjacencyMatrix[i] = make([]int, numNodes)
-	}
+func (g *BagGraph) FindAllBagsContaining(bag string) []string {
+	fromId := g.bagIds[bag]
+	parentIds := make([]int, 0, len(g.adjacencyMatrix))
+	g.traverseParents(fromId, &parentIds)
 
-	bagIds := make(map[string]int, numNodes)
-	bagNames := make(map[int]string, numNodes)
-	for i, bag := range bags {
-		bagIds[bag] = i
-		bagNames[i] = bag
+	parentNames := make([]string, len(parentIds))
+	for i, parentId := range parentIds {
+		parentNames[i] = g.bagNames[parentId]
 	}
+	return parentNames
+}
 
-	return BagGraph{
-		bagIds:          bagIds,
-		bagNames:        bagNames,
-		adjacencyMatrix: adjacencyMatrix,
+func (g *BagGraph) traverseParents(node int, alreadyVisited *[]int) {
+	nodeRow := g.adjacencyMatrix[node]
+	for parentId, quantity := range nodeRow {
+		if quantity > 0 && !arrayContains(*alreadyVisited, parentId) {
+			*alreadyVisited = append(*alreadyVisited, parentId)
+			g.traverseParents(parentId, alreadyVisited)
+		}
 	}
+}
+
+func arrayContains(list []int, x int) bool {
+	for _, y := range list {
+		if x == y {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
 	inputText := common.ReadAndSanitiseRows("input.txt")
+	rules, bagNames := parseRules(inputText)
+
+	graph := createGraph(bagNames)
+	for _, rule := range rules {
+		for _, childQuantity := range rule.childQuantities {
+			graph.AddEdge(rule.parentBag, childQuantity.child, childQuantity.quantity)
+		}
+	}
+
+	bagsContainingGold := graph.FindAllBagsContaining("shiny gold")
+	fmt.Println(len(bagsContainingGold), "bags can contain at least 1 shiny gold bag")
+}
+
+func parseRules(inputText []string) ([]Rule, []string) {
 	rules := make([]Rule, len(inputText))
 
 	bagNameSet := make(map[string]int, len(inputText))
@@ -73,16 +97,27 @@ func main() {
 		i++
 	}
 
-	graph := newGraph(bagNames)
-	for _, rule := range rules {
-		for _, childQuantity := range rule.childQuantities {
-			graph.addEdge(rule.parentBag, childQuantity.child, childQuantity.quantity)
-		}
+	return rules, bagNames
+}
+
+func createGraph(bags []string) BagGraph {
+	numNodes := len(bags)
+	adjacencyMatrix := make([][]int, numNodes)
+	for i := range adjacencyMatrix {
+		adjacencyMatrix[i] = make([]int, numNodes)
 	}
 
-	fmt.Println(graph.bagIds)
-	for _, row := range graph.adjacencyMatrix {
-		fmt.Println(row)
+	bagIds := make(map[string]int, numNodes)
+	bagNames := make(map[int]string, numNodes)
+	for i, bag := range bags {
+		bagIds[bag] = i
+		bagNames[i] = bag
+	}
+
+	return BagGraph{
+		bagIds:          bagIds,
+		bagNames:        bagNames,
+		adjacencyMatrix: adjacencyMatrix,
 	}
 }
 
